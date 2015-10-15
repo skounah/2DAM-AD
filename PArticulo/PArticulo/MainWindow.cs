a48dc9a704b158bdd5f82261db39cba6519754f6
skounah <el_skoner@hotmail.com>
2015-10-15 10:07:01 +0200

    PersisterHelper resuelto

--- PArticulo/PArticulo/MainWindow.cs
+++ PArticulo/PArticulo/MainWindow.cs
@@ -12,18 +12,13 @@ public partial class MainWindow: Gtk.Window
 		Build (); 
 
 		Console.WriteLine ("MainWndow constructor.");
-		IDbConnection dbconnection = App.Instance.DbConnection;
-		IDbCommand dbCommand = dbconnection.CreateCommand ();
-		dbCommand.CommandText = "select * from articulo";
-
-		//CREA EL DATAREADER
-		IDataReader dataReader = dbCommand.ExecuteReader ();
-
-
-		//COJE LAS COLUMNAS DEL TREEVIEW DE FORMA AUTOMATICA CON EL METODO GETCOLUMNAMES
-		string[] columnNames = getColumnNames (dataReader);
+		QueryResult queryResult = PersisterHelp.Get ("select * from articulo");
+		string[] columnNames = queryResult.ColumnNames;
 		CellRendererText cellRendererText = new CellRendererText ();
+
+
 		for (int index=0; index<columnNames.Length; index++) {
+	
 		// ESTABLECEMOS EL MODELO CON DELEGADO PARA LUEGO PODER TRATAR EL TEXTO RENDERIZADO
 			int column = index;
 			TreeView.AppendColumn (columnNames [index], cellRendererText, 
@@ -32,58 +27,21 @@ public partial class MainWindow: Gtk.Window
 				if (row[column] == DBNull.Value)
 					cellRendererText.Text = "sin asignar" ;
 				else 
-					//string value = tree_model.GetValue(iter, column).ToString();
 					cellRendererText.Text= row[column].ToString();
 			});
 		}
 
-
-
-		//Type[] types = getTypes (dataReader.FieldCount);
+	
 		ListStore listStore = new ListStore (typeof(IList));
 
-
-		//listStore = new ListStore (typeof(String), typeof(String), typeof (String), typeof (String));
-
-
-		//COJE LAS FILAS AUTOMATICAMENTE CON EL METODO GETVALUES
-		while (dataReader.Read()) {
-			//string[] values = getValues (dataReader);
-			IList values= getValues (dataReader);
-			listStore.AppendValues (values);
-
-		}
-		dataReader.Close ();
-		TreeView.Model = listStore;
+		foreach (IList row in (queryResult.Rows))
+			listStore.AppendValues (row);
+		TreeView.Model= listStore;
+		
+	
 		App.Instance.DbConnection.Close ();
 	}
 
-
-	//METODO QUE COJE EL NOMBRES DE LAS COLUMNAS DE LA BD Y LAS PASA A UN ARRAY
-	private string[] getColumnNames(IDataReader dataReader){
-		List<string> columnNames = new List<string> ();
-		int contador = dataReader.FieldCount;
-		for (int i=0;i<contador;i++)
-			columnNames.Add (dataReader.GetName (i));
-		return columnNames.ToArray();
-	}
-
-
-	//METODO QUE COJE LOS TIPOS DE LA BD Y LOS PASA A UN ARRAY DE TIPOS CON TODO STRING
-	private Type [] getTypes(int count){
-			List<Type> types = new List<Type>();
-		for (int i=0; i<count; i++) 
-			types.Add (typeof(String));
-	    return types.ToArray ();
-	}
-	//METODO QUE DEVUELVE UN STRING CON LOS VALORES DE LAS FILAS TRANSFORMADOS A ARRAY 
-	private IList getValues(IDataReader dataReader){
-		List<object> values = new List<object> ();
-		int count = dataReader.FieldCount;
-		for (int i=0;i<count;i++)
-			values.Add (dataReader [i].ToString ());
-		return values;
-	}
 
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
---
+++ PArticulo/PArticulo/QueryResult.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace PArticulo
+{
+	public class QueryResult
+	{
+		private string[] columnNames;
+		public string[] ColumnNames{
+			get {return columnNames;}
+			set {columnNames = value;}
+
+	}
+		private IEnumerable<IList> rows;
+		public IEnumerable<IList> Rows {
+			get{return rows;}
+			set{ rows = value;}
+		}
+	}
+}
+
