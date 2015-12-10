61e20657144d93fe9d72ead27869c512acce3f6f
skounah <el_skoner@hotmail.com>
2015-12-10 10:21:45 +0100

    Instalacion y primer proyecto con Eclipse

---
+++ GHolamundo/src/main/java/org/institutoserpis/ad/Holamundo.java
@@ -0,0 +1,10 @@
+package org.institutoserpis.ad;
+
+public class Holamundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Skoner te saludo desde Eclipse, pero desde un proyecto Graddle");
+
+	}
+
+}
