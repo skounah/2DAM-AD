e3ca80bfb165256754652df847b3d4495e4a1dfb
skounah <el_skoner@hotmail.com>
2016-01-19 09:03:33 +0100

    Modificados algunos metodos

--- GMysql/src/main/java/org/institutoserpis/ad/ArticuloPrueba.java
+++ GMysql/src/main/java/org/institutoserpis/ad/ArticuloPrueba.java
@@ -2,37 +2,38 @@ package org.institutoserpis.ad;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
-
 import com.mysql.jdbc.PreparedStatement;
-
 import java.sql.Statement;
-//import com.mysql.jdbc.Statement;
 
 
 public class ArticuloPrueba {
+	//CONEXION
 	static Connection connection;
-	static PreparedStatement insertPreparedStatement;
-	static PreparedStatement updatePreparedStatement;
-	static PreparedStatement deletePreparedStatement;
-	static PreparedStatement selectPreparedStatement;
+	//SENTENCIAS SQL
+	static PreparedStatement insertPreparedStatement; //INSERT
+	static PreparedStatement updatePreparedStatement; //UPDATE
+	static PreparedStatement deletePreparedStatement; //DELETE
+	static PreparedStatement selectPreparedStatement; //SELECT
+	static PreparedStatement selectAllPreparedStatement;//SELECTALL
 	
-	/* DATOS DE ARTICULO + METODO MOSTRAR
+	// DATOS DE ARTICULO + METODO MOSTRAR ARTICULO
 	String nombre;
 	int categoria;
 	double precio;
 	int id;
 	
-	MOSTRAR ARTICULO
+	//MOSTRAR ARTICULO
 	public static void showArticulo(ArticuloPrueba articulo){
  	System.out.println("Id: " +articulo.id);
 	System.out.println("Nombre: " +articulo.nombre);
 	System.out.println("Categoria: " +articulo.categoria);
-	Sytem.out.println("Precio: " +articulo.precio);
-	}*/
+	System.out.println("Precio: " +articulo.precio);
+	}
 	
-	//METODO CONEXION CON BD
+	//METODO PARA CONECTARSE A LA BD
 	public static Connection conection() throws SQLException{
 		 connection = DriverManager.getConnection(
 				"jdbc:mysql://localhost/bdpruebas", "root", "sistemas");
@@ -40,66 +41,152 @@ public class ArticuloPrueba {
 		return connection;
 	}
 	
-	//METODO SELECCION
-	public static void select() throws Throwable{ //LOS TRHOWS DE EXCEPCIONES SON PARA TIPOS DE DATOS INTRODUCIDOS(SCAN) INCORRECTOS.
-		Scanner scan = new Scanner(System.in);
+	public static void cerrarStatements() throws SQLException{
+		if(insertPreparedStatement!=null)
+			insertPreparedStatement.close();
 		
-		System.out.println("Introduce el id del Articulo que quieres consultar :");
-		int id= scan.nextInt();
-		String consulta = "select * from articulo where id=" +id;
+		if(updatePreparedStatement!=null)
+			insertPreparedStatement.close();
+		
+		if(deletePreparedStatement!=null)
+			insertPreparedStatement.close();
+		
+		if(selectPreparedStatement!=null)
+			insertPreparedStatement.close();
+		
 	}
 	
-	//METODO PARA INSERTAT A BD
-	public static void insert() throws SQLException{
-		Scanner scann = new Scanner(System.in);
-		String nuevosql = "insert into articulo (nombre, categoria, precio) values (?,?,?)";
+	//METODO SELECCION //REVISAAAAAR PECADOOR!!!!!
+	public static void select() throws SQLException{
+		Scanner scan = new Scanner(System.in);
 		
-		String nombre;
-		System.out.print("Introduzca nombre del nuevo articulo: ");	
-		nombre = scann.nextLine();
-		
-		int categoria;
-		System.out.print("Introduzca categoria del nuevo articulo. [1, 2, 3]: ");
-		categoria = scann.nextInt();
+		System.out.println("--------------------------------------------------");
+		System.out.println("Introduce el id del Articulo que quieres consultar :");
+		int id= scan.nextInt();
+		String selectsql = "select * from articulo where id = ?" ;
+		if(selectPreparedStatement == null)
+			selectPreparedStatement = (PreparedStatement) connection.prepareStatement(selectsql);
+		selectPreparedStatement.setInt(1, id);
+		ResultSet result= selectPreparedStatement.executeQuery(selectsql);
+		//ACABAR
+		System.out.println("-----------------------");
+		System.out.println("    ID    : " +result.getInt("id"));
+		System.out.println("  Nombre  : " +result.getString("nombre"));
+		System.out.println("Categoria : " +result.getInt("categoria"));
+		System.out.println("  Precio  : " +result.getDouble("precio"));
+		System.out.println("-----------------------");
 	
-		double precio;
-		System.out.print("Introduzca precio del nuevo articulo: ");
-		precio = scann.nextDouble();
-			
-		insertPreparedStatement = (PreparedStatement) connection.prepareStatement(nuevosql);
-		insertPreparedStatement.setString(1, nombre);
-		insertPreparedStatement.setInt(2, categoria);
-		insertPreparedStatement.setDouble(3, precio);
-		insertPreparedStatement.executeUpdate();
-		System.out.println("Articulo Guardado Correctamente");
 		
+	}
+	
+	//METODO PARA INSERTAR A BD
+	public static void insert() throws SQLException{
+		try{
+			Scanner scann = new Scanner(System.in);
+			String nuevosql = "insert into articulo (nombre, categoria, precio) values (?,?,?)";
+			
+			System.out.println("------------------------------------------");
+			String nombre;
+			System.out.print("Introduzca nombre del nuevo articulo: ");	
+			nombre = scann.nextLine();
+		
+			int categoria;
+			System.out.print("Introduzca categoria del nuevo articulo. [1, 2, 3]: ");
+			categoria = scann.nextInt();
+	
+			double precio;
+			System.out.print("Introduzca precio del nuevo articulo: ");
+			precio = scann.nextDouble();
+			
+			//CAMBIAMOS LOS ??? DE NUEVOSQL POR LOS VALORES INTRODUCIDOS POR TECLADO //MODELO PARA SENTENCIAS SQL
+			if(insertPreparedStatement==null)
+				insertPreparedStatement = (PreparedStatement) connection.prepareStatement(nuevosql);
+			insertPreparedStatement.setString(1, nombre);
+			insertPreparedStatement.setInt(2, categoria);
+			insertPreparedStatement.setDouble(3, precio);
+			insertPreparedStatement.executeUpdate();
+			System.out.println("Articulo Guardado Correctamente");
+		}catch (InputMismatchException e){ // EXCEPCION QUE SALTA AL INTRODUCIR UN TIPO INCORRECTO EN UN SCANNER
+			System.out.println("Introduce un tipo de dato valido");
+		}	
 	}
 	
 	//METODO PARA ACTUALIZAR LA BD
 	public static void update()throws Throwable{ //LOS TRHOWS DE EXCEPCIONES SON PARA TIPOS DE DATOS INTRODUCIDOS(SCAN) INCORRECTOS.
-		System.out.println("Introduce el id del Articulo que quieres modificar :");
+		String updatesql ="update articulo set nombre = ?, categoria = ?, precio = ? where id = ?";
+		Scanner scann = new Scanner(System.in);
+		
+		//System.out.println("--------------------------------------------------------------");
+		try{
+			int id;
+			System.out.println("Introduce el id del Articulo que quieres modificar :");
+			id= scann.nextInt();
+			scann.nextLine();
+			
+			String nombre;
+			System.out.println("Introduzca nuevo nombre del articulo: ");	
+			nombre = scann.nextLine();
+	
+			int categoria;
+			System.out.println("Introduzca nueva categoria del articulo. [1, 2, 3]: ");
+			categoria = scann.nextInt();
+			
+			double precio;
+			System.out.println("Introduzca nuevo precio del nuevo articulo: ");
+			precio = scann.nextDouble();
+		
+			if(updatePreparedStatement==null)
+				updatePreparedStatement = (PreparedStatement) connection.prepareStatement(updatesql);
+			updatePreparedStatement.setString(1, nombre);
+			updatePreparedStatement.setInt(2, categoria);
+			updatePreparedStatement.setDouble(3, precio);
+			updatePreparedStatement.setInt(4, id);
+			int count = updatePreparedStatement.executeUpdate();
+			
+			if (count == 1)
+				System.out.println("Articulo Actualizado Correctamente");
+			else 
+				System.out.println("No hay ningun Articulo con ese ID");
+		}catch (InputMismatchException e){ // EXCEPCION QUE SALTA AL INTRODUCIR UN TIPO INCORRECTO EN UN SCANNER
+			System.out.println("Introduce un tipo de dato valido");
+		}	
 	}
 	
 	//METODO PARA BORRAR DE LA BD
 	public static void delete()throws Throwable{ //LOS TRHOWS DE EXCEPCIONES SON PARA TIPOS DE DATOS INTRODUCIDOS(SCAN) INCORRECTOS.
-		System.out.println("Introduce el id del Articulo que quieres eliminar :");
+		String deletesql= "delete from articulo where id = ?";
+		Scanner scann = new Scanner(System.in);
+		
+		try{
+			int id;
+			System.out.println("Introduce el id del Articulo que quieres Borrar :");
+			id= scann.nextInt();
+			
+			deletePreparedStatement.setInt(1, id);
+			int count = deletePreparedStatement.executeUpdate();
+			
+			if (count == 1)
+				System.out.println("Articulo Borrado Correctamente");
+			else 
+				System.out.println("No hay ningun Articulo con ese ID");
+		}catch (InputMismatchException e){ // EXCEPCION QUE SALTA AL INTRODUCIR UN TIPO INCORRECTO EN UN SCANNER
+			System.out.println("Introduce un tipo de dato valido");
+		}	
+			
 	}
 	
 	//METODO PARA VER TODA UNA TABLA DE LA BD
-	public static void selectAll(){
+	public static void selectAll() throws SQLException{
 		System.out.println("Todos los articulos disponibles :");
-		String vertodos = "select * from articulo";
+		String selectAllsql = "select * from articulo";
+		selectAllPreparedStatement = (PreparedStatement) connection.prepareStatement(selectAllsql);
+		ResultSet result= selectAllPreparedStatement.executeQuery(selectAllsql);
+		//ACABAR
 	}
 	
 	
 	public static void main(String[] args) throws Throwable {
-		// SIN METODO CONECT
-		/*Connection connection = DriverManager.getConnection(
-			"jdbc:mysql://localhost/bdpruebas", "root", "sistemas");
-		Statement stat = connection.createStatement();*/
-		
-		conection(); // CON METODO CONNECT
-		
+		conection(); 
 		//// MENU PRINCIPAL - CONTROL SOBRE LA BD ////
 		Scanner leer = new Scanner(System.in);
 		int opcion=-1;
@@ -112,8 +199,9 @@ public class ArticuloPrueba {
 			System.out.println("4-Eliminar.");
 			System.out.println("5-Ver todos.");
 			System.out.println("6-Salir.");
-			System.out.println("-------------------");
+			System.out.println("-----------------------");
 			System.out.print("SELECCIONE UNA OPCION : ");
+
 			try{
 				opcion = leer.nextInt();
 			}catch (InputMismatchException e){
@@ -138,9 +226,8 @@ public class ArticuloPrueba {
 				break;
 			case 6:
 				System.out.println("Cerrando conexión con la BD.");
-				//connection.close(); SIN METODO CONECT
-				conection().close();//CON METODO CONECT
-				//PreparedStatement.close();
+				cerrarStatements();//CIERRA STATEMENTS 
+				conection().close();//CIERRA CONEXION
 				System.out.println("Finalizada la conexión con BDpruebas.");
 				break;
 			default:
