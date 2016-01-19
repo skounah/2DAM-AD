343f871523a29311ed083c744e0be76e6852424b
skounah <el_skoner@hotmail.com>
2016-01-19 09:28:03 +0100

    No Crear Proyectos Java - Commit hecho para corregir el anterior

--- PAsino/src/org/institutoserpis/ad/HolaMundo.java
+++ PAsino/src/org/institutoserpis/ad/HolaMundo.java
@@ -1,9 +1,9 @@
 package org.institutoserpis.ad;
 
 public class HolaMundo {
-
+	//NO CREAR PROYECTOS JAVA SI NO GRADLE
 	public static void main(String[] args) {
-		System.out.println("Hola Skoner te saludo desde Eclipse ");
+		System.out.println("Hola Skoner te saludo desde Eclipse. Evita crear proyectos que no sean gradle ");
 
 	}
 
