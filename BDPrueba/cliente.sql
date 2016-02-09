89dc87ce6046420a26de1b8f032c2725d5e5fba0
skounah <el_skoner@hotmail.com>
2016-02-09 09:09:37 +0100

    Base de Datos Actualizada,  Clientes, Pedido, LineaPedido. Con sus Relaciones

---
+++ BDPrueba/cliente.sql
@@ -0,0 +1,5 @@
+insert into cliente (nombre) values ('cliente 1');
+insert into cliente (nombre) values ('cliente 2');
+insert into cliente (nombre) values ('cliente 3');
+insert into cliente (nombre) values ('victor romero');
+
--- BDPrueba/tablas.sql
+++ BDPrueba/tablas.sql
@@ -4,7 +4,38 @@ nombre varchar(50) not null unique,
 categoria bigint,
 precio decimal(10,2)
 );
+
 create table categoria (
+id bigint auto_increment primary key,
+nombre varchar(50) null unique
+);
+
+create table cliente (
 id bigint auto_increment primary key,
 nombre varchar(50) not null unique
 );
+
+create table pedido (
+id bigint auto_increment primary key,
+cliente bigint not null,
+fecha datetime not null,
+importe decimal(10,2)
+);
+
+create table lineapedido (
+id bigint auto_increment primary key,
+articulo bigint not null,
+precio decimal(10,2),
+unidades decimal(10,2),
+importe decimal(10,2)
+);
+
+alter table articulo add foreign key (categoria) references categoria (id);
+
+alter table pedido add foreign key (cliente) references cliente (id);
+
+alter table lineapedido add foreign key (pedido) references pedido (id) on delete cascade;
+
+alter table lineapedido add foreign key (articulo) references articulo (id);
+
+
